70da5632802245cc21fac1a1941a51f0c9fa1c61
Leonardo Romeiro <leozinwork@gmail.com>
2024-02-27 20:14:44 -0300

    Testando a extensão da lista

---
+++ UtmBuilder.Core.Test/Extensions/ListExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtmBuilder.Core.Extensions;
+
+namespace UtmBuilder.Core.Test.Extensions
+{
+    [TestClass]
+    public class ListExtensionsTests
+    {
+        [TestMethod]
+        public void NaoAdicionaNaListaQuandoValorNulo()
+        {
+            var list = new List<string>();
+            list.AddIfNotNull("utm_sourne", null);
+            Assert.AreEqual(0, list.Count);
+        }
+        [TestMethod]
+        public void AdicionaNaLista()
+        {
+            var list = new List<string>();
+            list.AddIfNotNull("utm_sourne", "sourne");
+            Assert.AreEqual(1, list.Count);
+        }
+    }
+}
